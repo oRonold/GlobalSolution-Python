4858a225a14356c381d1dabb3eca10c41dab6ba7
oRonold <or040918@gmail.com>
2023-05-29 21:20:49 -0300

    atualizando função cadastro doador

--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,14 @@
 def cadastro_doador():
-    print('Bem-vindo a sessão de cadastro de doadores!')
+    print('Bem-vindo a sessão de cadastro temporario de doadores!')
     print('Por favor preencha os campos abaixo\n')
 
     nome_doador = input('Digite seu nome completo: ')
     data_nascimento = int(input('Digite a sua data de nascimento: '))
     cpf = input('Digite seu CPF: ')
-    regiao = input('Digite sua regiao em que voce mora em São Paulo: ')
+    regiao = input('Digite sua regiao em que voce mora em São Paulo: \n')
+    print(f'Parabens {nome_doador} o seu cadastro foi finalizado! Segue as informações abaixo: \n')
+    print(f'Data de nascimento: {data_nascimento}')
+    print(f'CPF: {cpf}')
+    print(f'Região cadastrada: {regiao}')
+    print('Cadastro finalizado! A seguir voce podera continuar para a doação ou sair, caso escolha sair o cadastro precisara ser feito novamente, pois ele é temporario')
+    sim_nao = input('Voce deseja continuar para a doação?(s/n) ')
