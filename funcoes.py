675392e1e7dc36d4fb8e314706c0794e3be8ee20
oRonold <or040918@gmail.com>
2023-05-29 21:44:46 -0300

    atualizações finais doações

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,5 @@
 def cadastro_doador_e_doacao():
-    print('Bem-vindo a sessão de cadastro temporario de doadores!')
+    print('Bem-vindo a sessão de cadastro temporario de doadores de São Paulo!')
     print('Por favor preencha os campos abaixo\n')
 
     nome_doador = input('Digite seu nome completo: ')
@@ -19,4 +19,14 @@ def cadastro_doador_e_doacao():
         print('A seguir voce podera realizar a sua doação para a ONG mais proxima da sua região\n')
         alimento = input('Qual alimento voce quer doar? ')
         qtd = float(input('Qual a quantidade em KG? '))
-
+        regiao_doacao = input('Digite em qual região voce quer que a sua doação seja redirecionada em São Paulo:(norte/sul/leste/oeste) ')
+        if regiao_doacao == 'sul':
+            print('Perfeito, a sua doação sera direcionada para o galpão da Cruz Vermelha Brasileira')
+        elif regiao_doacao == 'oeste':
+            print('Excelente, a sua doação sera direcionada para o depósito da FUSSP(Fundo Social de São Paulo)')
+        elif regiao_doacao == 'leste':
+            print('Ótimo, a sua doação será direcionada para a Associação Fenix, na zona leste')
+        elif regiao_doacao == 'norte':
+            print('Excelente, a sua doação será direcionada para o Banco de Alimentos localizado na zona Norte')
+        
+        transporte = input('Agora o transporte do alimento, a sua preferencia é ')
